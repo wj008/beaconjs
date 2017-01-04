f77354c4973276785bcfb028728ea178d6a835ff
wj008 <26029682@qq.com>
2017-01-05 02:19:31 +0800

    测试session

--- apps/home/controllers/index.ctl.ts
+++ apps/home/controllers/index.ctl.ts
@@ -2,8 +2,9 @@ import {Beacon} from "../../../src/core/beacon";
 export class Index extends Beacon.Controller {
 
     public async indexAction() {
-
-        this.assign('title', 'hello sdopx');
+        await this.initSesion();
+        console.log(this.getSession('abc'));
+        this.assign('title', this.getSession('abc'));
         this.assign('foot_content', 'All rights reserved.');
         this.assign('meetingPlace', 'New York');
       //  throw new Error('数据异常请重试.');
@@ -12,6 +13,9 @@ export class Index extends Beacon.Controller {
     }
 
     public async loginAction() {
+        await this.initSesion();
+        console.log(Date.now() - this.context.startTime);
+        this.setSession('abc','xxxxxx');
         this.end('login');
     }
 }
