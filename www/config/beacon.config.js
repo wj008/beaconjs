27f4012f948f8be7aaaf670eb2563391f609fc8d
wj008 <26029682@qq.com>
2017-01-15 02:21:28 +0800

    调整目录结构

--- www/config/beacon.config.js
+++ www/config/beacon.config.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = {
-    default_encoding: 'utf-8',//默认编码
-    default_content_type: 'text/html' //默认输出类型
+    default_encoding: 'utf-8'//默认编码
+    , default_content_type: 'text/html' //默认输出类型
+    , enable_json_ext: true
 };
