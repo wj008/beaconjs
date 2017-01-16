518ab7cddc5128998b73aff63d6defcc00744bb1
wj008 <26029682@qq.com>
2017-01-16 15:49:49 +0800

    修复规则

--- app.ts
+++ app.ts
@@ -2,19 +2,13 @@ import http = require('http');
 import {Beacon} from "./src/core/beacon";
 import {RedisSession} from "./src/adapter/session/redis";
 import serveStatic = require('serve-static');
-
 const hostname = '127.0.0.1';
 const port = 3001;
-
-
 Beacon.regSessionType('redis', RedisSession);
-
 Beacon.debug = true;
 Beacon.init();
-
-
-console.log(Beacon.RUNTIME_PATH);
-console.log(Beacon.VIEW_PATH);
+//console.log(Beacon.RUNTIME_PATH);
+//console.log(Beacon.VIEW_PATH);
 //ddddoo
 Beacon.Route.loadRoute('admin');
 Beacon.Route.loadRoute('home');
@@ -30,6 +24,7 @@ const server = http.createServer((req, res) => {
         });
     });
 });
+
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
