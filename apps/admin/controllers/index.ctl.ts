91143a67c20e6335a49a0f0162d98e84ef7e1251
wj008 <26029682@qq.com>
2017-01-16 15:50:02 +0800

    修复规则

--- apps/admin/controllers/index.ctl.ts
+++ apps/admin/controllers/index.ctl.ts
@@ -11,6 +11,19 @@ export class Index extends AdminController {
         console.log(Date.now() - this.context.startTime);
     }
 
+    public async leftAction() {
+        let pid = this.get('pid:n', 0);
+        let info = await this.db.getRow('select * from @pf_sysmenu where id=?', pid);
+        this.assign('info', info);
+        let rows = await this.db.getList('select * from @pf_sysmenu where pid=? and allow=1 order by sort asc', pid);
+        for (let i = 0; i < rows.length; i++) {
+            let row = rows[i];
+            row.childs = await this.db.getList('select * from @pf_sysmenu where pid=? and allow=1 order by sort asc', row.id);
+        }
+        this.assign('rows', rows);
+        this.display('left');
+    }
+
     public async logoutAction() {
         this.delSession();
         this.redirect('__APPROOT__/index.html');
