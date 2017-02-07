b9f23db4971acefa972c043ef157a47264916527
wj008 <26029682@qq.com>
2017-02-08 01:21:31 +0800

    调整代码

--- apps/admin/controllers/index.ctl.ts
+++ apps/admin/controllers/index.ctl.ts
@@ -1,7 +1,6 @@
 import {AdminController} from "./admin.ctl";
 export class Index extends AdminController {
 
-
     public async indexAction() {
         let rows = await this.db.getList('select * from @pf_sysmenu where pid=0 and allow=1 order by sort asc');
         this.assign('rows', rows);
