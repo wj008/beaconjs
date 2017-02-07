7ec313dc9ba9cc315e66ec27c796cdf7c4433000
wj008 <26029682@qq.com>
2017-02-08 01:23:06 +0800

    添加支持 asset 标记扩展

--- src/adapter/sdopx/sdopx_ext.ts
+++ src/adapter/sdopx/sdopx_ext.ts
@@ -9,6 +9,32 @@ Sdopx.registerFilter('pre', function (content: string, sdopx: Sdopx) {
     return content;
 });
 
+//替换资源
+Sdopx.registerFilter('output', function (content: string, sdopx: Sdopx) {
+    if (sdopx.context) {
+        let html = [];
+        let asset = sdopx.context.getAsset();
+        asset.css.forEach(function (item) {
+            html.push(`<link rel="stylesheet" type="text/css" href="${item}" />`);
+        });
+        asset.js.forEach(function (item) {
+            html.push(`<script type="text/javascript" src="${item}"></script>`);
+        });
+        if (/<!--#asset#-->/.test(content)) {
+            content = content.replace(/<!--#asset#-->/,html.join("\n"));
+        }
+        else if (/(<\/head>)/i.test(content)) {
+            content = content.replace(/<\/head>/i, function ($0) {
+                return html.join("\n") + "\n" + $0;
+            });
+        }
+        else {
+            content += html.join("\n");
+        }
+    }
+    return content;
+});
+
 //注册uri 函数
 Sdopx.registerPlugin('uri', function (params, out, sdopx) {
     if (!sdopx.context) {
@@ -33,4 +59,25 @@ Sdopx.registerPlugin('uri', function (params, out, sdopx) {
     url = url.substring(1);
     out.raw(sdopx.context.url(url, args, app));
     return;
+});
+
+//注册资源引用
+Sdopx.registerPlugin('asset', function (params, out, sdopx) {
+    if (!sdopx.context) {
+        return;
+    }
+    if (sdopx.context._asset == null) {
+        out.raw('<!--#asset#-->');
+    }
+    if (!params.src && typeof(params.src) != 'string') {
+        return;
+    }
+    let name = params.src;
+    let depend = params.depend || null;
+    if (depend) {
+        sdopx.context.addAsset(name, depend);
+    } else {
+        sdopx.context.addAsset(name);
+    }
+
 });
