1aff50a2d5e010e83fae28d50394dbceb8269eb7
wj008 <26029682@qq.com>
2017-02-12 02:53:15 +0800

    调整代码

--- www/config/asset.config.js
+++ www/config/asset.config.js
@@ -10,10 +10,13 @@ module.exports = {
         'jquery-ui': '../jquery-ui/jquery-ui',
         'yee': 'yee',
         'yee-validate': 'yee.validate-2.0.1',
+        'layer': '../layer/layer',
+        'yee-confirm': 'yee.confirm',
     },
     depends: {
         'jquery-ui': ['../jquery-ui/custom.css', 'jquery'],
         'yee': ['jquery'],
         'yee-validate': ['yee'],
+        'yee-confirm': ['yee', 'layer'],
     }
 };
