ebd43c4760dfff1858e6f1ea65f90b186b888f97
wj008 <26029682@qq.com>
2017-02-12 17:56:59 +0800

    调整代码

---
+++ apps/admin/controllers/start_city.ctl.ts
@@ -0,0 +1,84 @@
+import {AdminController} from "./admin.ctl";
+import {PageList} from "../../libs/pagelist";
+
+export class StartCity extends AdminController {
+
+    public async indexAction() {
+        let plist = new PageList('select * from @pf_start_city order by sort asc');
+        let {info, list} = await plist.getData(this);
+        this.assign('list', list);
+        this.assign('pdata', info);
+        this.display('startcity');
+    }
+
+    public async addAction() {
+        if (this.isGet()) {
+            let row = await this.db.getRow('select `sort` from @pf_start_city  order by `sort` desc limit 0,1');
+            let sort = row ? row.sort + 10 : 10;
+            this.assign('row', {sort: sort});
+            this.display('startcity_add.form');
+            return;
+        }
+        if (this.isPost()) {
+            let {name = ''}=this.post();
+            let sort = this.post('sort:n', 0);
+            if (name == '') {
+                this.fail('出发城市不可为空', {name: '出发城市不可为空'});
+            }
+            let vals = {
+                name: name,
+                sort: sort,
+            }
+            await this.db.insert('@pf_start_city', vals);
+            this.success('添加出发城市成功');
+        }
+    }
+
+    public async editAction() {
+        let id = this.param('id:n', 0);
+        if (!id) {
+            this.fail('参数有误');
+        }
+        if (this.isGet()) {
+            let row = await this.db.getRow('select * from @pf_start_city where id=?', id);
+            this.assign('row', row);
+            this.display('startcity_edit.form');
+            return;
+        }
+        if (this.isPost()) {
+            let {name = ''}=this.post();
+            let sort = this.post('sort:n', 0);
+            if (name == '') {
+                this.fail('出发城市不可为空', {name: '出发城市不可为空'});
+            }
+            let vals = {
+                name: name,
+                sort: sort
+            }
+            await this.db.update('@pf_start_city', vals, id);
+            this.success('编辑出发城市成功');
+        }
+    }
+
+    public async delAction() {
+        let id = this.param('id:n', 0);
+        if (!id) {
+            this.fail('参数有误');
+        }
+        await this.db.delete('@pf_start_city', id);
+        this.success('删除出发城市成功');
+    }
+
+    public async sortAction() {
+        let id = this.param('id:n', 0);
+        if (!id) {
+            this.fail('参数有误');
+        }
+        let sort = this.param('sort:n', 0);
+        await this.db.update('@pf_start_city', {
+            sort: sort
+        }, id);
+        this.success('更新排序成功');
+    }
+
+}
