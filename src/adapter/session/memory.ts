0d6ed9063a9096edc12d880564dd87a49988c21d
wj008 <26029682@qq.com>
2017-02-16 00:26:07 +0800

    优化效率

--- src/adapter/session/memory.ts
+++ src/adapter/session/memory.ts
@@ -6,20 +6,25 @@ import {SessionBase} from "../../core/session_base";
 
 export class MemorySession implements SessionBase {
 
-    public timeout = 3600;
+    public static timeout = null;
+    public static nextCheckTime = 0;
+    public static checkTime = 300;
     public static store = {};
     private _data = null;
     private _isInit = false;
     private _cookie = null;
 
     public constructor() {
-        let options = Beacon.getConfig('session:*');
-        this.timeout = options.timeout || 3600;
+        if (!MemorySession.timeout) {
+            let options = Beacon.getConfig('session:*');
+            MemorySession.timeout = options.timeout || 3600;
+            MemorySession.checkTime = options.checkTime || 300;
+        }
     }
 
     public async init(cookie: string) {
         this._cookie = cookie;
-        let data = MemorySession.store[cookie] || { data: {}, expire: 0 };
+        let data = MemorySession.store[cookie] || {data: {}, expire: 0};
         this._data = Beacon.clone(data);
         this._isInit = true;
     }
@@ -44,7 +49,7 @@ export class MemorySession implements SessionBase {
         }
         this._data = this._data || {data: {}, expire: 0};
         this._data.data[name] = value;
-        this._data.expire = Date.now() + this.timeout * 1000;
+        this._data.expire = Date.now() + MemorySession.timeout * 1000;
     }
 
     public delete(name?: string) {
@@ -61,12 +66,16 @@ export class MemorySession implements SessionBase {
             delete store[this._cookie];
             return;
         }
-        this._data.expire = Date.now() + this.timeout * 1000;
+        this._data.expire = Date.now() + MemorySession.timeout * 1000;
         store[this._cookie] = this._data;
     }
 
     public static gc() {
         let now = Date.now();
+        if (MemorySession.nextCheckTime > now) {
+            return;
+        }
+        MemorySession.nextCheckTime = now + MemorySession.checkTime * 1000;
         let store = MemorySession.store;
         for (let key in  store) {
             let item = store[key];
